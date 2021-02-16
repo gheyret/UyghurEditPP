f730eed303da8cf73d4caf07381645d96151ad3d
Uyghur <gheyret@gmail.com>
2021-02-16 14:32:21 +0900

    Add files via upload

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) 2014 AlphaSierraPapa for the SharpDevelop Team
+// Copyright (c) 2014 AlphaSierraPapa for the SharpDevelop Team
 // 
 // Permission is hereby granted, free of charge, to any person obtaining a copy of this
 // software and associated documentation files (the "Software"), to deal in the Software
@@ -15,9 +15,7 @@
 // FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
 // OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 // DEALINGS IN THE SOFTWARE.
-
 #region Using directives
-
 using System;
 using System.Resources;
 using System.Windows;
@@ -25,25 +23,17 @@ using System.Reflection;
 using System.Windows.Markup;
 
 #endregion
-
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-
-[assembly: CLSCompliant(true)]
-
-
-[assembly: ThemeInfo(
-	ResourceDictionaryLocation.None, //where theme specific resource dictionaries are located
-									 //(used if a resource is not found in the page,
-									 // or application resource dictionaries)
-	ResourceDictionaryLocation.SourceAssembly //where the generic resource dictionary is located
-											  //(used if a resource is not found in the page,
-											  // app, or any theme specific resource dictionaries)
+[assembly: CLSCompliant (true)]
+[assembly: ThemeInfo (ResourceDictionaryLocation.None, //where theme specific resource dictionaries are located
+//(used if a resource is not found in the page,
+// or application resource dictionaries)
+ResourceDictionaryLocation.SourceAssembly//where the generic resource dictionary is located
+//(used if a resource is not found in the page,
+// app, or any theme specific resource dictionaries)
 )]
-
-
-
 [assembly: AssemblyTitle ("UyghurEditPP")]
 [assembly: AssemblyDescription ("")]
 [assembly: AssemblyConfiguration ("")]
@@ -52,15 +42,11 @@ using System.Windows.Markup;
 [assembly: AssemblyCopyright ("Copyright 2021")]
 [assembly: AssemblyTrademark ("")]
 [assembly: AssemblyCulture ("")]
-[assembly: System.Runtime.InteropServices.ComVisible(false)]
-
-
-[assembly: XmlnsDefinition("http://icsharpcode.net/sharpdevelop/avalonedit", "UyghurEditPP")]
-[assembly: XmlnsDefinition("http://icsharpcode.net/sharpdevelop/avalonedit", "UyghurEditPP.Editing")]
-[assembly: XmlnsDefinition("http://icsharpcode.net/sharpdevelop/avalonedit", "UyghurEditPP.Rendering")]
-[assembly: XmlnsDefinition("http://icsharpcode.net/sharpdevelop/avalonedit", "UyghurEditPP.Highlighting")]
-[assembly: XmlnsDefinition("http://icsharpcode.net/sharpdevelop/avalonedit", "UyghurEditPP.Search")]
-
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2243:AttributeStringLiteralsShouldParseCorrectly",	Justification = "AssemblyInformationalVersion does not need to be a parsable version")]
-
-[assembly: AssemblyVersion ("0.1.*")]
+[assembly: System.Runtime.InteropServices.ComVisible (false)]
+[assembly: XmlnsDefinition ("http://icsharpcode.net/sharpdevelop/avalonedit", "UyghurEditPP")]
+[assembly: XmlnsDefinition ("http://icsharpcode.net/sharpdevelop/avalonedit", "UyghurEditPP.Editing")]
+[assembly: XmlnsDefinition ("http://icsharpcode.net/sharpdevelop/avalonedit", "UyghurEditPP.Rendering")]
+[assembly: XmlnsDefinition ("http://icsharpcode.net/sharpdevelop/avalonedit", "UyghurEditPP.Highlighting")]
+[assembly: XmlnsDefinition ("http://icsharpcode.net/sharpdevelop/avalonedit", "UyghurEditPP.Search")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage ("Microsoft.Usage", "CA2243:AttributeStringLiteralsShouldParseCorrectly", Justification = "AssemblyInformationalVersion does not need to be a parsable version")]
+[assembly: AssemblyVersion ("0.2.*")]
